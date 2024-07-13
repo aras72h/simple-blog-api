cbc6f85b2d9f8a6d57e8e6e88eb000a146fe4982
Arash <aras72h@gmail.com>
2024-07-14 01:32:14 +0330

    Use blog model in blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,14 +1,12 @@
 const pool = require('../config/db');
+const Blog = require('../models/blog');
 
 exports.createBlog = async (req, res) => {
     const { title, content } = req.body;
 
     try {
-        const result = await pool.query(
-            'INSERT INTO blogs (user_id, title, content) VALUES ($1, $2, $3) RETURNING *',
-            [req.userId, title, content]
-        );
-        res.status(201).json(result.rows[0]);
+        const newBlog = await Blog.create(title, content, req.userId);
+        res.status(201).json(newBlog);
     } catch (err) {
         res.status(500).json({ error: 'Internal server error' });
     }
@@ -18,10 +16,7 @@ exports.likeBlog = async (req, res) => {
     const blogId = req.params.id;
 
     try {
-        await pool.query(
-            'INSERT INTO user_like_blog (user_id, blog_id) VALUES ($1, $2)',
-            [req.userId, blogId]
-        );
+        await Blog.like(req.userId, blogId);
         res.status(200).json({ message: 'Blog post liked' });
     } catch (err) {
         res.status(500).json({ error: 'Internal server error' });
@@ -30,14 +25,8 @@ exports.likeBlog = async (req, res) => {
 
 exports.listBlogs = async (req, res) => {
     try {
-        const result = await pool.query(
-            `SELECT blogs.*, COUNT(user_like_blog.blog_id) AS likes
-             FROM blogs
-             LEFT JOIN user_like_blog ON blogs.id = user_like_blog.blog_id
-             GROUP BY blogs.id
-             ORDER BY blogs.created_at DESC`
-        );
-        res.status(200).json(result.rows);
+        const blogs = await Blog.list();
+        res.status(200).json(blogs);
     } catch (err) {
         res.status(500).json({ error: 'Internal server error' });
     }
