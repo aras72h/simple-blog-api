4632354464a6ac5410235cdcf83006e43eaa980e
Arash <aras72h@gmail.com>
2024-07-14 01:22:19 +0330

    Add comments

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,14 +7,14 @@ exports.verifyToken = (req, res, next) => {
         return res.status(403).json({ error: 'No token provided' });
     }
 
-    const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7, authHeader.length) : authHeader;
+    const token = authHeader.split(' ')[1]; // Extract the token from "Bearer token"
 
     jwt.verify(token, secret, (err, decoded) => {
         if (err) {
             return res.status(500).json({ error: 'Failed to authenticate token' });
         }
 
-        req.userId = decoded.userId;
-        next();
+        req.userId = decoded.userId; // Attach user ID to request
+        next(); // Proceed to the next middleware or route handler
     });
 };
